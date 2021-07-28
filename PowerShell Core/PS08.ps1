83351eac318c98438d0758633b29f5a4aee3d206
Adam Baxter <11729388+ABratMaxed@users.noreply.github.com>
2021-07-28 18:35:47 +1000

    Update PS08.ps1

--- PowerShell Core/PS08.ps1
+++ PowerShell Core/PS08.ps1
@@ -1,2 +1,6 @@
-Start-Sleep -Seconds 5
-Write-Output "Hello World 8"
+Start-Sleep -Seconds 5;
+Write-Output "Hello World 8";
+Write-Output $Param1;
+Write-Output $Param2;
+Write-Output $SourceConnection.Options.ConnectionString;
+Write-Output $TargetConnection.Options.ConnectionString;
