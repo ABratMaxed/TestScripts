7a8ec087335fffdb483dd986fc4f652a612ac7cb
Adam Baxter <11729388+ABratMaxed@users.noreply.github.com>
2021-07-28 18:34:54 +1000

    Update PS06.ps1

--- PowerShell Core/PS06.ps1
+++ PowerShell Core/PS06.ps1
@@ -1,2 +1,6 @@
-Start-Sleep -Seconds 5
-Write-Output "Hello World 6"
+Start-Sleep -Seconds 5;
+Write-Output "Hello World 6";
+Write-Output $Param1;
+Write-Output $Param2;
+Write-Output $SourceConnection.Options.ConnectionString;
+Write-Output $TargetConnection.Options.ConnectionString;
