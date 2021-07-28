d59d7628388a2eccb48dbd4a52ac0d28a906c182
Adam Baxter <11729388+ABratMaxed@users.noreply.github.com>
2021-07-28 18:35:14 +1000

    Update PS07.ps1

--- PowerShell Core/PS07.ps1
+++ PowerShell Core/PS07.ps1
@@ -1,2 +1,6 @@
-Start-Sleep -Seconds 5
-Write-Output "Hello World 7"
+Start-Sleep -Seconds 5;
+Write-Output "Hello World 7";
+Write-Output $Param1;
+Write-Output $Param2;
+Write-Output $SourceConnection.Options.ConnectionString;
+Write-Output $TargetConnection.Options.ConnectionString;
