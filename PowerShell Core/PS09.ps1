1c42890a8064fce5762f40da9073f3659d9a6dca
Adam Baxter <11729388+ABratMaxed@users.noreply.github.com>
2021-07-28 18:36:02 +1000

    Update PS09.ps1

--- PowerShell Core/PS09.ps1
+++ PowerShell Core/PS09.ps1
@@ -1,2 +1,6 @@
-Start-Sleep -Seconds 5
-Write-Output "Hello World 9"
+Start-Sleep -Seconds 5;
+Write-Output "Hello World 9";
+Write-Output $Param1;
+Write-Output $Param2;
+Write-Output $SourceConnection.Options.ConnectionString;
+Write-Output $TargetConnection.Options.ConnectionString;
