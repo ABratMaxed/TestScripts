04419186dafc3e814d686d50c7e136d89dbf8126
Adam Baxter <11729388+ABratMaxed@users.noreply.github.com>
2021-07-29 12:36:22 +1000

    Update PS10.ps1

--- PowerShell Core/PS10.ps1
+++ PowerShell Core/PS10.ps1
@@ -5,5 +5,5 @@ Write-Output $Param1;
 Write-Output $Param2;
 Write-Output $SourceConnection.Options.ConnectionString;
 Write-Output $TargetConnection.Options.ConnectionString;
-"@ | | Out-File PS10.ps1
+"@ | Out-File PS10.ps1
 pwsh PS10.ps1;
