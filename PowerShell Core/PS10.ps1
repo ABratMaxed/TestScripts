a402c99ea96bfa9c2d496bf369c2ba57ae46bef1
Adam Baxter <11729388+ABratMaxed@users.noreply.github.com>
2021-07-29 12:49:25 +1000

    Update PS10.ps1

--- PowerShell Core/PS10.ps1
+++ PowerShell Core/PS10.ps1
@@ -1,9 +1,9 @@
 @"
 Start-Sleep -Seconds 5;
 Write-Output "Hello World 10";
-Write-Output $Param1;
-Write-Output $Param2;
-Write-Output $SourceConnection.Options.ConnectionString;
-Write-Output $TargetConnection.Options.ConnectionString;
+Write-Output `$Param1;
+Write-Output `$Param2;
+Write-Output `$SourceConnection.Options.ConnectionString;
+Write-Output `$TargetConnection.Options.ConnectionString;
 "@ | Out-File PS10.ps1
 pwsh PS10.ps1;
